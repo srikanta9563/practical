6f35f3b492f645a221c546e86c4897cfbba0cd7a
srikanta9563 <110465259+srikanta9563@users.noreply.github.com>
2022-08-09 17:44:38 +0530

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,16 @@
+public class calculator {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 3;
+        int c = a+b;
+        int d = a-b;
+        int e = a/b;
+        int f = a*b;
+
+        System.out.println("The two numbers are:"+a+","+b);
+        System.out.println("the sum is "+ c);
+        System.out.println("the subtraction is "+ d);
+        System.out.println("the divison is "+ e);
+        System.out.println("the multiplicaton is "+ f);
+    }
+}
