33f71cd2276164dea0879c46b7d7f874b58ad708
srikanta9563 <110465259+srikanta9563@users.noreply.github.com>
2022-08-10 16:02:24 +0530

    Add files via upload

---
+++ rectangular.java
@@ -0,0 +1,13 @@
+public class rectangular {
+
+    public static void main(String[] args) {
+    int length= 4;
+    int breadth= 2;
+        System.out.println("the lenghth and breadth are "+length+","+breadth);
+    int peremeter= 2*(length+breadth);
+    int area= length*breadth;
+        System.out.println("the peremeter is rectangular is "+peremeter);
+        System.out.println("the area is rectangular is "+area);
+
+    }
+}
