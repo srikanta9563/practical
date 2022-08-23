13da0cd68120c57bc244615d17777a33a4657aa0
srikanta9563 <110465259+srikanta9563@users.noreply.github.com>
2022-08-23 19:16:59 +0530

    Add files via upload

---
+++ simpleifstatement.java
@@ -0,0 +1,12 @@
+public class simpleifstatement {
+    public static void main(String[] args) {
+        int x = 5;
+        int y= 3;
+        if (x+y<10){
+            System.out.println("x+y is less than 10");
+
+
+        }
+    }
+
+}
