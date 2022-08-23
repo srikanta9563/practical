97c413bf0b1bcb26a28e66a4dfa0fce8d4f9457f
srikanta9563 <110465259+srikanta9563@users.noreply.github.com>
2022-08-23 19:27:16 +0530

    Add files via upload

---
+++ switchtest.java
@@ -0,0 +1,23 @@
+public class switchtest {
+    public static void main(String[] args) {
+        int num = 10 ;
+        switch (num){
+            case 1 :
+                System.out.println("12");
+                break;
+            case 12:   System.out.println("15");
+            break;
+            case 10:   System.out.println("very good");
+            break;
+            case 15:  System.out.println("20");
+            break;
+            default:
+                System.out.println("number is not within");
+
+        }
+
+
+    }
+
+}
+
