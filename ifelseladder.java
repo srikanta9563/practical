8a5acd465146762971c16754b146e574a96cd43f
srikanta9563 <110465259+srikanta9563@users.noreply.github.com>
2022-08-23 19:20:56 +0530

    Add files via upload

---
+++ ifelseladder.java
@@ -0,0 +1,25 @@
+public class ifelseladder {
+    public static void main(String[] args) {
+        String name= "srikanta";
+        if (name =="Sourav") {
+            System.out.println("name is sourav");
+        }
+            else if (name=="arko") {
+            System.out.println("name is arko");
+        }
+                else if (name=="halu") {
+            System.out.println("name is halu");
+        }
+                else {
+            System.out.println(name);
+        }
+
+
+
+
+            }
+        }
+
+
+
+
