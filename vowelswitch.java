1185994f5a540ae6f756006a9bf9f9d2fa53f0d0
srikanta9563 <110465259+srikanta9563@users.noreply.github.com>
2022-08-25 14:23:19 +0530

    Add files via upload

---
+++ vowelswitch.java
@@ -0,0 +1,18 @@
+public class vowelswitch{
+    public static void main(String[] args) {
+        char ch= 'e';
+        switch (ch){
+            case 'y':
+                System.out.println("its not a vowl");
+                break;
+            case 'e' :   System.out.println("its a vowl");
+            break;
+            case 'o':   System.out.println("its not a vowl");
+            break;
+            default:   System.out.println("consonant");
+
+        }
+
+
+    }
+}
