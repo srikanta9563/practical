6af3400cc70d1cb66ec9f8b420582b4b2ef5efe0
srikanta9563 <110465259+srikanta9563@users.noreply.github.com>
2022-08-30 23:53:02 +0530

    Add files via upload

---
+++ staticmethod.java
@@ -0,0 +1,10 @@
+public class staticmethod {
+    public static void main(String[] args) {
+        display();
+
+    }
+    static void display() {// static method
+        System.out.println("My name is Srikanta");
+
+}}
+
