2d778e93c24a28c2855765f7308230dafeed742f
srikanta9563 <110465259+srikanta9563@users.noreply.github.com>
2022-08-31 00:34:04 +0530

    Add files via upload

---
+++ dowhileloop.java
@@ -0,0 +1,12 @@
+public class dowhileloop {
+    public static void main(String[] args) {
+        int i=0;
+        System.out.println(" 1st 10 even numbers");
+        do {
+            System.out.println(i);
+            i = i + 2;
+        }while (i <= 10) ;
+
+        }
+    }
+
