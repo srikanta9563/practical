3c7cb8bef6ca57d8324d20c254c7c7d2a0a8db83
srikanta9563 <110465259+srikanta9563@users.noreply.github.com>
2022-08-30 23:51:21 +0530

    Add files via upload

---
+++ predefinedmethod.java
@@ -0,0 +1,7 @@
+public class predefinedmethod {
+    //using maximum and minimum method
+    public static void main(String[] args) {
+        System.out.println("the max number is:"+Math.max(5,8));
+        System.out.println("thn min number is:"+Math.min(5,8));
+    }
+}
