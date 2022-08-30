5707c119c7b794866eb788426042163c899bd111
srikanta9563 <110465259+srikanta9563@users.noreply.github.com>
2022-08-30 14:21:26 +0530

    Add files via upload

---
+++ studenttest.java
@@ -0,0 +1,23 @@
+ class Studenttest1{
+     // field
+     int age;
+     String name;
+ } // create another class
+    class Studenttest{
+        //main method
+        public static void main(String[] args) {
+            //creating object
+            Studenttest1 st=new Studenttest1();
+            //print it
+            System.out.println(st.age);
+            System.out.println(st.name);
+
+        }
+
+
+
+
+    }
+
+
+
