882505e4dcbc21afa11632dce698d7bcb1b44969
srikanta9563 <110465259+srikanta9563@users.noreply.github.com>
2022-09-26 22:07:24 +0530

    Add files via upload

---
+++ arrayex5.java
@@ -0,0 +1,9 @@
+//array index bound
+public class arrayex5 {
+    public static void main(String[] args) {
+        int[] a={1,3,4,5,6};
+        for (int i=0;i<=a.length;i++){
+            System.out.println(a[i]);
+        }
+    }
+}
