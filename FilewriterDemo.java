71eb600b9c434bd8b597f85ad89879fd0244c037
srikanta9563 <110465259+srikanta9563@users.noreply.github.com>
2022-11-17 22:31:33 +0530

    Add files via upload

---
+++ FilewriterDemo.java
@@ -0,0 +1,24 @@
+import java.io.FileReader;
+import java.io.FileWriter;
+
+public class FilewriterDemo {
+
+    public static void main(String[] args) {
+        try {
+            FileWriter obj1 = new FileWriter("D:test.txt");
+            obj1.write("Welcome to anudip foundation");
+            FileReader obj2 = new FileReader("D:test.txt");
+
+            int i = 0;
+            while((i=obj2.read()) != -1) {	//condition for reading file; if there is nothing in the file it will return '-1'
+                System.out.print(((char)i));	//typecasting the int 'j' to char
+            }
+
+            System.out.println("done");
+            obj1.close();
+            obj2.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
